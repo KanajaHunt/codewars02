61b36b9180a3bcec116eb3e927c7437ac477ca53
Kanaja Hunt <KanajaHunt@gmail.com>
2024-01-26 13:48:37 -0500

    correct answer - though too much into it

---
+++ no-loop.js
@@ -0,0 +1,16 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+// solution
+
+// map -> item, includes
+// arr -> mult elements, case sen
+// value -> "". ""
+// return boolean
+
+function containValue(a,x){
+    return a.includes(x) ? true : false;
+}
