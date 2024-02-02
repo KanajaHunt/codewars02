3955e0d8dfe788e488f16d6d340541287ece3e14
Kanaja Hunt <KanajaHunt@gmail.com>
2024-02-01 19:53:52 -0500

    saw answer - trip us when 1+ param

---
+++ max-array.js
@@ -0,0 +1,31 @@
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+
+// maxProduct ({-4, -27, -15, -6, -1}, 2) ==> return (4)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is  -4 * -1 = 4 .
+
+function maxNums(numbers, size){
+    //   b - a === big to small (highest to lowest)
+    const sortNums = numbers.sort((a,b) => b - a);
+    // since we alr have it sorted u want slice to pick up the 1st num to the size provided 
+    // .slice(start, end) vs .splice(start, deleteCount)
+    return sortNums.slice(0, size).reduce((acc,c) => acc * c); 
+}
