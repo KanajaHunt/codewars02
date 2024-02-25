820aa58aed0e031b219d69a2033fd45013fcc617
Kanaja Hunt <KanajaHunt@gmail.com>
2024-02-25 14:32:20 -0500

    first try

---
+++ reduce-but-grow.js
@@ -0,0 +1,6 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// my solution
+const multArr = nums => nums.reduce((acc, c) => acc * c);
